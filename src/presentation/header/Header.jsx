d77abb8ee1e9c3c009a476a430cbe6324ea357e0
Codyvegas <codyjohnson2015@yahoo.com>
2022-11-08 18:22:52 -0800

    header constructed

--- src/presentation/header/Header.jsx
+++ src/presentation/header/Header.jsx
@@ -3,34 +3,31 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="overflow-auto">
+    <div className="">
       <div className=" button-container mt-2 flex flex-col items-end  ">
-        {/* <div className="max-w-sm text-2xl font-semibold text-green-800"> */}
-        {/* <div className="max-w-sm py-2 text-2xl font-semibold text-green-800"> */}
-        {/* <Link to="/proof-of-work"> */}
-        <button className="mr-80 max-w-xs rounded bg-green-50 py-2 text-2xl font-semibold text-green-800  active:bg-red-600">
-          Proof Of Work
-        </button>
-        {/* </Link> */}
-        {/* </div> */}
-        {/* <div className="max-w-sm py-2 text-2xl font-semibold text-green-800"> */}
-        {/* <Link to="/about"> */}
-        <button className=" mr-60 max-w-xs rounded bg-green-50 py-2 text-2xl font-semibold text-green-800  active:bg-red-600">
-          About
-        </button>
-        {/* </Link> */}
-        {/* </div> */}
-        {/* <div className="max-w-sm py-2 text-2xl font-semibold text-green-800"> */}
-        {/* <Link to="/contact-me"> */}
-        <button className=" mr-10 max-w-xs rounded bg-green-50 py-2 text-2xl font-semibold text-green-800  active:bg-red-600">
-          Contact Me
-        </button>
-        {/* </Link> */}
-        {/* </div> */}
+        <Link to="/proof-of-work">
+          <button className=" mr-20 max-w-xs rounded-md px-8 py-2 pt-2 text-2xl font-semibold text-green-700 hover:text-green-600 active:bg-red-400">
+            Proof Of Work
+          </button>
+        </Link>
+        <Link to="/about">
+          <button className=" mr-12 max-w-xs rounded-md px-8 py-2 pt-2 text-2xl font-semibold text-green-700 hover:text-green-600 active:bg-red-400">
+            Contact Me
+          </button>
+        </Link>
+        <Link to="/contact-me">
+          <button className=" mr-6 max-w-xs rounded-md px-8 py-2 pt-2 text-2xl font-semibold text-green-700 hover:text-green-600 active:bg-red-400">
+            About
+          </button>
+        </Link>
       </div>
-      <div className="background-img">
-        <img className="scale-70 h-80  min-w-full" src={mainLogo} alt=""></img>
-      </div>
+      {/* <div className="background-img"> */}
+      <img
+        className="min-w-full rounded-xl drop-shadow-xl hover:bg-blue-600"
+        src={mainLogo}
+        alt=""
+      ></img>
+      {/* </div> */}
     </div>
   );
 }
--- src/presentation/pages/landingpage/LandingPage.jsx
+++ src/presentation/pages/landingpage/LandingPage.jsx
@@ -1,7 +1,17 @@
 import headshot from "../../../assets/headshot.png";
 
 function LandingPage() {
-  return <div className="vh- bg-green-900"></div>;
+  return (
+    <div className="landing-page vh- bg-green-900">
+      <div className="codys-blur">
+        <img
+          className="my-headshot"
+          src={headshot}
+          alt="A picture of me!"
+        ></img>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
