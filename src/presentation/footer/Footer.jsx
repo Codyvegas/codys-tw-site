5f41de3cc9ffa8779840c11bea66d6c6b72706ab
Codyvegas <codyjohnson2015@yahoo.com>
2022-11-10 16:01:23 -0800

    landing page style

--- src/presentation/footer/Footer.jsx
+++ src/presentation/footer/Footer.jsx
@@ -1,17 +1,16 @@
 import vegas from "../../assets/vegas.png";
-import email from "../../assets/email.png";
 
 function Footer() {
   return (
     <div className=" h-30 bg-black">
       <div className="info-container flex h-32 items-center justify-around">
-        <h1 className="  text-xl text-yellow-600">Las Vegas, NV</h1>
+        <h1 className="  text-xl text-sky-400">Las Vegas, NV</h1>
         <img
           className="ml-32 h-32"
           src={vegas}
           alt="The Best City in the World!"
         ></img>
-        <h1 className=" text-xl text-yellow-600">Codyjohnson2015@yahoo.com</h1>
+        <h1 className=" text-xl text-sky-400">Codyjohnson2015@yahoo.com</h1>
       </div>
     </div>
   );
--- src/presentation/pages/landingpage/LandingPage.jsx
+++ src/presentation/pages/landingpage/LandingPage.jsx
@@ -2,15 +2,22 @@ import headshot from "../../../assets/headshot.png";
 
 function LandingPage() {
   return (
-    <div className="landing-page vh- bg-green-900">
-      <div className="codys-blur">
-        <h1 className="">Hi, Im Cody!</h1>
-        <p>An avid golfer, fitness enthusiast, and hazy IPA conneseiur</p>
-        <img
-          className="my-headshot"
-          src={headshot}
-          alt="A picture of me!"
-        ></img>
+    <div className="landing-page bg-slate-700">
+      <div className="m-auto flex w-6/12 flex-col	">
+        <div className="my-24	 rounded-3xl border-2 border-solid border-cyan-700 bg-black/50 ">
+          <h1 className="mt-12 mb-2 text-center text-2xl	text-cyan-400">
+            Hi, Im Cody!
+          </h1>
+          <p className="text-center 	text-2xl text-cyan-400">
+            An avid golfer, fitness enthusiast, and Hazy IPA connoisseur!{" "}
+            <br></br>I'm excited to show you what ive been working on!
+          </p>
+          <img
+            className=" ml-20 mb-12	mt-6 rounded-3xl bg-gradient-to-r from-cyan-400 to-blue-800"
+            src={headshot}
+            alt="A picture of me!"
+          ></img>
+        </div>
       </div>
     </div>
   );
