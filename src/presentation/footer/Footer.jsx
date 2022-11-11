d937b393fc8929edb20d904b3071e78053ea024c
Codyvegas <codyjohnson2015@yahoo.com>
2022-11-10 16:42:51 -0800

    header adjustment

--- src/presentation/footer/Footer.jsx
+++ src/presentation/footer/Footer.jsx
@@ -4,13 +4,13 @@ function Footer() {
   return (
     <div className=" h-30 bg-black">
       <div className="info-container flex h-32 items-center justify-around">
-        <h1 className="  text-xl text-sky-400">Las Vegas, NV</h1>
+        <h1 className=" text-lg text-sky-400">Las Vegas, NV</h1>
         <img
-          className="ml-32 h-32"
+          className=" h-32"
           src={vegas}
           alt="The Best City in the World!"
         ></img>
-        <h1 className=" text-xl text-sky-400">Codyjohnson2015@yahoo.com</h1>
+        <h1 className=" text-lg text-sky-400">Codyjohnson2015@yahoo.com</h1>
       </div>
     </div>
   );
--- src/presentation/header/Header.jsx
+++ src/presentation/header/Header.jsx
@@ -4,24 +4,24 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="">
-      <div className="button-container  mt-2 flex flex-col items-end  ">
+      <div className="flex justify-end">
         <Link to="/proof-of-work">
-          <button className="mr-20 max-w-xs rounded-md px-8 py-2 pt-2 text-2xl font-semibold text-green-700 hover:text-green-600 active:bg-red-400">
+          <button className=" rounded-md px-8 py-2 text-xl font-semibold text-black  hover:text-blue-600 active:bg-teal-400">
             Proof Of Work
           </button>
         </Link>
         <Link to="/about">
-          <button className=" mr-12 max-w-xs rounded-md px-8 py-2 pt-2 text-2xl font-semibold text-green-700 hover:text-green-600 active:bg-red-400">
+          <button className="rounded-md px-8 py-2 text-xl font-semibold text-black hover:text-blue-600 active:bg-teal-400">
             Contact Me
           </button>
         </Link>
         <Link to="/contact-me">
-          <button className=" mr-6 max-w-xs rounded-md px-8 py-2 pt-2 text-2xl font-semibold text-green-700 hover:text-green-600 active:bg-red-400">
+          <button className="rounded-md px-8 py-2 text-xl font-semibold text-black hover:text-blue-600 active:bg-teal-400">
             About
           </button>
         </Link>
       </div>
-      <div className="background-img ">
+      <div className="background-img">
         <img
           className="min-w-full rounded-xl drop-shadow-xl"
           src={mainLogo}
