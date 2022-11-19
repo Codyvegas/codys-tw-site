e56d5700251b17bdde5870bbeece9a378530d80c
Codyvegas <codyjohnson2015@yahoo.com>
2022-11-18 18:05:17 -0800

    finish styling on proof page

--- src/presentation/pages/proofPage/ProofPage.jsx
+++ src/presentation/pages/proofPage/ProofPage.jsx
@@ -8,7 +8,7 @@ function Proof() {
     <div className="bg-slate-800">
       <div className="proof-page-container flex bg-slate-800">
         <div className=" m-12  w-1/3 justify-center rounded-2xl border-2 border-solid border-purple-700 bg-slate-900 px-12 text-xl text-white drop-shadow-xl hover:shadow-2xl">
-          <h1 className="my-6 text-center text-2xl text-purple-400">
+          <h1 className=" my-10 truncate text-center text-2xl text-purple-400">
             <b>Abstract Herbs</b>
           </h1>{" "}
           <img
@@ -18,16 +18,19 @@ function Proof() {
           <div className="container my-6">
             <p className="text-2xl text-white">
               This was my first time building a website using Reactjs. Its safe
-              to say I love the way React works. Abstract Herbs is my favorite
-              NFT project built on the Ethereum Blockchain, and the creator is
-              from Berlin! We connected on Twitter and he gave me the chance to
-              do the website for the project! This site is going to be in
+              to say I love the way React works. <br></br>
+              <br></br>
+              Abstract Herbs is my favorite NFT project built on the Ethereum
+              Blockchain, and the creator is from Berlin!
+              <br></br>
+              <br></br>We connected on Twitter and he gave me the chance to do
+              the website for the project! This site is going to be in
               production very soon...
             </p>
           </div>
         </div>
         <div className=" my-12 mb-auto w-1/3 justify-center rounded-2xl border-2 border-solid border-orange-500 bg-slate-900 px-12 text-xl text-white drop-shadow-xl hover:shadow-2xl  ">
-          <h1 className="my-6 text-center text-2xl text-red-300">
+          <h1 className="my-10 truncate text-center text-2xl text-red-300 outline-white">
             <b>Grip Gods</b>
           </h1>{" "}
           <img
@@ -38,9 +41,10 @@ function Proof() {
             <p className="text-2xl text-white">
               A dual purpose project! For one, its the first time working with
               another developer on a website and I learned a ton about mr's and
-              branching. Secondly, it was amazing to implement a passion (golf)
-              of mine into a project! Who knows, this business may actually go
-              live one day.
+              branching. <br></br>
+              <br></br>Secondly, it was amazing to implement a passion (golf) of
+              mine into a project! Who knows, this business may actually go live
+              one day.
             </p>
             <img
               src={customerform}
@@ -49,7 +53,7 @@ function Proof() {
           </div>
         </div>
         <div className="m-12 mb-auto w-1/3 justify-center rounded-2xl border-2 border-solid border-blue-600 bg-slate-900 px-12 text-xl text-white drop-shadow-xl hover:shadow-2xl">
-          <h1 className="my-6 text-center text-2xl text-cyan-400">
+          <h1 className="my-10 mb-auto truncate text-center text-2xl text-cyan-400">
             <b>My Portfolio Website</b>
           </h1>{" "}
           <img
@@ -60,10 +64,17 @@ function Proof() {
             <p className="text-2xl text-white">
               My personal website incorporates a brand new framework, Tailwind
               CSS! This was a bit of a learning curve but once I spent some time
-              with it, the framework speaks for itself. My personal opinion is
-              that you should learn vanilla CSS first, make sure your
-              foundational principles are there, and then try something like
-              Tailwind.
+              with it, the framework speaks for itself.
+              <br></br>
+              <br></br>
+              My personal opinion is that you should learn vanilla CSS first,
+              make sure your foundational principles are there, and then try
+              something like Tailwind.
+              <br></br>
+              <br></br>
+              In order to get these websites drawn up, I decided to use a Figma
+              design. This allows me to stay in line with the designers work and
+              also my own ideas.
             </p>
           </div>
         </div>
